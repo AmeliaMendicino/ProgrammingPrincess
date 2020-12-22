69ad0c09b65f8f15dd3b8f1eb020107aa0239348
Amelia Mendicino <amelia.mendicino@gmail.com>
2020-12-21 23:19:46 -0700

    Set Gem Shape and Color
    Setting the gem's shape and color based on the variable's type and kind,
    so it's easy for the user to see what types their variables are and hopefully
    the shape helps them get an idea of the difference between things like
    primitives and objects, or being able to reassign to a variable in JavaScript.

--- components/VariableGem.tsx
+++ components/VariableGem.tsx
@@ -1,5 +1,8 @@
 import { FunctionComponent } from "react";
 import { HexagonGem } from "./Gems/HexagonGem";
+import { OctogonGem } from "./Gems/OctogonGem";
+import { DiamondGem } from "./Gems/DiamondGem";
+import { TriGem } from "./Gems/TriGem";
 
 import styles from "../styles/Gems.module.css";
 
@@ -20,15 +23,67 @@ export const VariableGem: FunctionComponent<GemProps> = ({
   kind,
   type,
 }: GemProps) => {
-  const color = "blue";
-  console.log(kind, type);
+  let isPrimitive = true;
+  let color;
+  let GemShape;
+  let displayValue = value;
+
+  switch (type) {
+    case "":
+    case "undefined":
+      color = "gray";
+      break;
+    case "boolean":
+      color = "purple";
+      break;
+    case "number":
+      color = "blue";
+      break;
+    case "bigint":
+      color = "navy";
+      displayValue = value.toString();
+      break;
+    case "string":
+      color = "pink";
+      break;
+    case "symbol":
+      color = "yellow";
+      break;
+    case "function":
+      color = "green";
+      isPrimitive = false;
+      break;
+    case "object":
+    default:
+      isPrimitive = false;
+      if (Array.isArray(value)) {
+        color = "red";
+      } else if (value === null) {
+        color = "black";
+      } else {
+        color = "orange";
+      }
+      break;
+  }
+
+  switch (kind) {
+    case "const":
+      GemShape = isPrimitive ? HexagonGem : OctogonGem;
+      break;
+    case "let":
+    case "var":
+    default:
+      GemShape = isPrimitive ? TriGem : DiamondGem;
+      break;
+  }
+
   return (
     <figure className={styles.container}>
       <figure className={styles.gem}>
         <figcaption className={styles.gemText}>
-          {JSON.stringify(value)}
+          {JSON.stringify(displayValue)}
         </figcaption>
-        <HexagonGem color={color} />
+        <GemShape color={color} />
       </figure>
       <figcaption className={[styles.variableText, styles[`${color}Text`]].join(" ")}>
         {name}
